f56782bab98d8a49faae42a40cae4a4e0dee01e2
hyugowacha <uena0506@gmail.com>
2024-12-24 20:22:30 +0900

    로고 디자인 쬐끔 바꿈

--- main.cs
+++ main.cs
@@ -17,7 +17,6 @@ namespace ConsoleProject
 
             backgroundManager backgroundmanager = new backgroundManager();
             backgroundmanager.Title();
-            backgroundmanager.Textbox();
         }
     }
 }
