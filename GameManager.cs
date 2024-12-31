b4d6a4afc620d3cc6da0d60d5997668c822a9674
hyugowacha <uena0506@gmail.com>
2024-12-31 10:37:05 +0900

    잠깐 임시로 바꿈

--- GameManager.cs
+++ GameManager.cs
@@ -20,29 +20,30 @@ namespace ConsoleProject
             String Playername;
             Region region = new Region();
 
-            title.PrintTitle();
-            intro.CreatePlayer();
+            //title.PrintTitle();
+            //intro.CreatePlayer();
 
-            Playername = intro.playerName;
-            
-            if (Playername == "")
-            {
-                player = new Player(100, 20, 1, 10, 3000);
-            }
-            
-            else if(Playername != "")
-            {              
-                player = new Player(100, 20, 10, 1, 3000, Playername);
-            }
-
-            else
-            {
-                player = new Player(100, 20, 1, 10, 3000);
-            }
+            //Playername = intro.playerName;
+            //
+            //if (Playername == "")
+            //{
+            //    player = new Player(100, 20, 1, 10, 3000);
+            //}
+            //
+            //else if(Playername != "")
+            //{              
+            //    player = new Player(100, 20, 10, 1, 3000, Playername);
+            //}
+            //
+            //else
+            //{
+            //    player = new Player(100, 20, 1, 10, 3000);
+            //}
                        
             Item ekhorn = new Gun();
             Item basicArmour = new Armour();
 
+            player = new Player(100, 20, 10, 1, 3000);//임시
             player[0] = basicArmour;
             player[1] = ekhorn;
             basicArmour.ItemEffect(player);
